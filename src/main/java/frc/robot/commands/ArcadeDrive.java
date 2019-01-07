97f4d75e0c8ce5ca0c00f8d74bb56607f36e9e63
Lathi <byron.lathi@19-BL-170725.local>
2019-01-07 08:45:04 -0500

    Add Drivetrain subsystem and command

--- src/main/java/frc/robot/commands/ArcadeDrive.java
+++ src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -13,10 +13,10 @@ import frc.robot.Robot;
 /**
  * An example command.  You can replace me with your own command.
  */
-public class ExampleCommand extends Command {
-  public ExampleCommand() {
+public class ArcadeDrive extends Command {
+  public ArcadeDrive() {
     // Use requires() here to declare subsystem dependencies
-    requires(Robot.m_subsystem);
+    requires(Robot.m_drivetrain);
   }
 
   // Called just before this Command runs the first time
