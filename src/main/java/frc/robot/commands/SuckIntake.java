2415c9e5513f72f1d1ca5d36b381e9f33df6fbc9
bman12three4 <byron.lathi@student.lovett.org>
2019-01-07 21:28:38 -0500

    set end condition for intake

--- src/main/java/frc/robot/commands/SuckIntake.java
+++ src/main/java/frc/robot/commands/SuckIntake.java
@@ -41,6 +41,7 @@ public class SuckIntake extends Command {
   // Called once after isFinished returns true
   @Override
   protected void end() {
+    Robot.m_groundintake.wheels.set(0);
   }
 
   // Called when another command which requires one or more of the same
