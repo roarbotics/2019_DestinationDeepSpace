4edbe1e9aa922e92d98d169d9749960916cd2063
bman12three4 <byron.lathi@student.lovett.org>
2019-01-21 17:50:05 -0500

    remove old vision code (again)

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -183,7 +183,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousInit() {
-    m_visionChoice.getSelected().start();
+    //m_visionChoice.getSelected().start();
     // m_autonomousCommand = m_chooser.getSelected();
 
     /*
@@ -209,7 +209,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
-    m_visionChoice.getSelected().start();
+    //m_visionChoice.getSelected().start();
     // This makes sure that the autonomous stops running when
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
