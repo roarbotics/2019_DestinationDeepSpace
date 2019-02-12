eee772ef4f26fbf5529a0d845b285ba0a4339782
bman12three4 <byron.lathi@student.lovett.org>
2019-02-11 23:50:26 -0500

    remove unused import

--- src/main/java/frc/robot/commands/Push.java
+++ src/main/java/frc/robot/commands/Push.java
@@ -8,7 +8,6 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 
